876fce8354721a1c179f89e42207956e2e484b67
Vladislav <qedirast@gmail.com>
2021-01-09 15:02:35 +0700

    Added API application

---
+++ api/models/LoginForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace api\models;
+
+use common\models\Token;
+use common\models\User;
+use yii\base\Model;
+
+/**
+ * Login form
+ *
+ * @property-read null|User $user
+ */
+class LoginForm extends Model
+{
+    public $username;
+    public $password;
+
+    private $_user;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return [
+            // username and password are both required
+            [['username', 'password'], 'required'],
+            // password is validated by validatePassword()
+            ['password', 'validatePassword'],
+        ];
+    }
+
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword(string $attribute, array $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Incorrect username or password.');
+            }
+        }
+    }
+
+    /**
+     * @return Token|null
+     */
+    public function auth(): ?Token
+    {
+        if ($this->validate()) {
+            $token = new Token();
+            $token->user_id = $this->getUser()->id;
+            $token->generateToken(time() + 3600 * 24);
+            return $token->save() ? $token : null;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    protected function getUser(): ?User
+    {
+        if ($this->_user === null) {
+            $this->_user = User::findByUsername($this->username);
+        }
+
+        return $this->_user;
+    }
+}
